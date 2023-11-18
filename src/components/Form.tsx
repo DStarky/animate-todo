6245756abca2fecab9282bf94f0df3d96d233e54
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-18 10:53:47 +0500

    placeholder

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -28,9 +28,10 @@ const Form = ({ setList, list }: FormProps) => {
     <form className="flex w-full gap-3" onSubmit={submitHandler}>
       <input
         type="text"
-        className="flex-1 rounded border-[1px] border-zinc-800 px-3 py-2 focus-within:outline-none focus:ring-4 focus:ring-blue-300"
+        className="flex-1 rounded border-[1px] border-zinc-800 px-3 py-2 focus-within:outline-none focus:ring-4 focus:ring-blue-300 focus:placeholder:text-transparent"
         value={value}
         onChange={(e) => setValue(e.target.value)}
+        placeholder="Input your task here"
       />
       <button className="rounded bg-zinc-800 px-3 py-2 text-white focus:ring-4 focus:ring-blue-300">
         Submit new task
